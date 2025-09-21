8fb74a49e0d9b55f7a4e57259fd1ad0cbc1718f4
vihsantos <vick_santos_vick@hotmail.com>
2025-09-21 15:29:14 -0300

    fix: ajustando o card procedimento

--- lib/ui/core/ui/card_procedimento.dart
+++ lib/ui/core/ui/card_procedimento.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:sesi_check/ui/core/themes/app_colors.dart';
 
 class CardProcedimento extends StatelessWidget {
+  final String status;
   const CardProcedimento({
-    super.key,
+    super.key, required this.status,
   });
 
   @override
@@ -23,7 +25,28 @@ class CardProcedimento extends StatelessWidget {
           )
         ]
       ),
-      child: Text("data"),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(Icons.medication, color: AppColors.secundaria),
+              SizedBox(width: 10),
+              Text("PROCEDIMENTO", style: TextStyle(fontWeight: FontWeight.w500),),
+            ],
+          ),
+          Row(
+            children: [
+              Visibility(
+                visible: false,
+                child: Text(status, style: TextStyle(fontWeight: FontWeight.w500),)),
+              Icon(Icons.keyboard_arrow_right, color: AppColors.secundaria,),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
+
+//to do: trocar por expansion tile
