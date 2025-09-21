e20a1ce36928969e9ad7e613d7c854557d4a5996
vihsantos <vick_santos_vick@hotmail.com>
2025-09-21 09:56:47 -0300

    fix: adicionando mais cores

--- lib/ui/core/themes/app_colors.dart
+++ lib/ui/core/themes/app_colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 abstract final class AppColors {
   static const primaria = Color(0xFF0C4DA1);
   static const secundaria = Color(0xFF6FAB0D);
+  static const bottomSelected = Color(0xFF168821);
+  static const bottomUnselected = Color(0xFF96C59B);
 }
